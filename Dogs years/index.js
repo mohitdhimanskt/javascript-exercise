f9335c83f3fb1bfc37882b6f24b412c26ecdf651
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-04-13 22:28:56 +0530

    years

---
+++ Dogs years/index.js
@@ -0,0 +1,7 @@
+function calculateDogAge(age){
+  var dogYear = 7*age;
+   console.log ("Your doggie is" + "dogYear" + "years old in dog years!");
+}
+calculateDogAge(1);
+calculateDogAge(0.5);
+calculateDogAge(12);
