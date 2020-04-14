d189fa2e9afb7f31caf2c2f72cd8ab942253bbec
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-04-14 14:53:22 +0530

    initialcommit

---
+++ fortune teller/index.js
@@ -0,0 +1,11 @@
+    function tellFortune(future){
+    var numKids = 5;
+    var partner = "David Beckham";
+    var geolocation = "costa Rica" ;
+    var jobTitle =" web developer",
+    var randomNumger = Math.floor(Math.random()* 5)
+        var future = 'You will be a ' + jobTitle + ' in ' + location + ' and married to ' +
+    partner + ' ' + ' with ' + numKids + ' kids.'; 
+    }
+        console.log(future);
+    
